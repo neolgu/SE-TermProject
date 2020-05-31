33a1c2c71a67e8d4d288c1c92aa4157b06af6edf
neolgu <32592676+neolgu@users.noreply.github.com>
2020-05-31 17:30:14 +0900

    Flask & Firebase test
    Flask & Firebase, Web POST comunication test

--- SE_TermProject_backend/main.py
+++ SE_TermProject_backend/main.py
@@ -1,18 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+# firebase 연동
+cred = credentials.Certificate("se-back-end-firebase-adminsdk.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://se-back-end.firebaseio.com/'
+})
+
+# # 디비 위치 지정
+# ref = db.reference()
+# # 추가
+# ref.update({"이름": "값의 형태"})
+# # 받아오기
+# data = ref.get()
 
 app = Flask(__name__)
+
+
 @app.route("/")
-def hello():
-    return "<h1>Hello World!</h1>"
+def login():
+    return render_template('login.html')
 
-@app.route("/hello")
-def hello_flask():
-    return "<h1>Hello Flask!</h1>"
 
-@app.route("/first")
-def hello_first():
-    return "<h3>Hello First!</h3>"
+@app.route('/result', methods=['POST'])
+def post():
+    values = [request.form['id'], request.form['pw']]
+    asdf = ""
+    asdf += str(values[0]) + '\n'
+    asdf += str(values[1])
+
+    return asdf
+
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port="8080")
-
+    app.run(host='0.0.0.0', port="8080")
